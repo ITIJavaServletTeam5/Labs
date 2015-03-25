04afda3c4333425021d06f295d5c78d5dcb6336c
sharno <sharnoby3@gmail.com>
2015-03-25 08:53:54 +0200

    added Filters for Admin, Trainee and Instructor

---
+++ src/main/webapp/common/controller/InstructorFilter.java
@@ -0,0 +1,33 @@
+package common.controller;
+
+import pojo.User;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by sharno on 3/25/15.
+ */
+@WebFilter(filterName = "InstructorFilter", urlPatterns = "/instructor/*")
+public class InstructorFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpSession session = ((HttpServletRequest) request).getSession();
+        if (((User)session.getAttribute("user")).getRole().equals("INSTRUCTOR")) {
+            chain.doFilter(request, response);
+        } else {
+            ((HttpServletResponse)response).sendRedirect("/login");
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
